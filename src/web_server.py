28911c70b1f9028df20114860fa2c596d65fc90d
mihaier <mihaiershch@gmail.com>
2021-12-18 22:30:22 +0300

    Procfile web server changes

--- src/web_server.py
+++ src/web_server.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask import request
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def hellow_world():
     return "<p>Привет участникам электива<p>"
+
+@app.route("/parse/<job>")
+def parse_param(job):
+    return "Вы обратились на {}".format(job)
+
+@app.route("/sample_post", methods=["POST"])
+def parse_param_post():
+    d = json.loads(request.data)
+    return "Вы обратились по посту на {}".format(d.['body'])
 
 app.run()
