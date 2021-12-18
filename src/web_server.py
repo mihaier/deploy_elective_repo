e7b77a77c70aee6b4cdb7817761d789207fdd840
mihaier <mihaiershch@gmail.com>
2021-12-18 23:53:33 +0300

    get to model
    model details

--- src/web_server.py
+++ src/web_server.py
@@ -1,25 +1,32 @@
-
-   
+import pandas as pd
 from flask import Flask
 from flask import request
 import json
-
+import pickle
 
 app = Flask(__name__)
 
+with open('../models/rforestregressor.pkt', 'rb') as file:
+    random_forest_regressor = pickle.load(file)
 
-@app.route("/",methods=['POST'])
-def hello_world():
-    return "<p>Привет участникам электива"
+features_list = ['P1', 'P2', 'P3', 'P4',
+       'P5', 'P6', 'P7', 'P8', 'P9', 'P10', 'P11', 'P12', 'P13', 'P14', 'P15',
+       'P16', 'P17', 'P18', 'P19', 'P20', 'P21', 'P22', 'P23', 'P24', 'P25',
+       'P26', 'P27', 'P28', 'P29', 'P30', 'P31', 'P32', 'P33', 'P34', 'P35',
+       'P36', 'P37']
 
-@app.route("/parse/<job>")
-def parse_para(job):
-    return "Вы обратились на {}".format(job)
+@app.route("/", methods=['GET'])
+def predict(model=random_forest_regressor):
 
-@app.route("/sample_post",methods=["POST"])
-def parse_para_post():
-    d = json.loads(request.data)
-    return "Вы обратились по посту на {}".format(d['body'])
+    data_to_predict = pd.read_json(request.data)[features_list]
+
+    model_prediction = model.predict(data_to_predict)
+
+    prediction_json = json.dumps(model_prediction.to_list())
+
+    return prediction_json
 
 
-###app.run()
+app.run()
+
+
