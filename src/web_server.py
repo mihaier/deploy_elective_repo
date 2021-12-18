ad1af6cf0dbd4b47bd12f4d54ed6520914d5ae8c
mihaier <mihaiershch@gmail.com>
2021-12-18 23:21:42 +0300

    some changes
    some changes in web_server.py

--- src/web_server.py
+++ src/web_server.py
@@ -1,3 +1,5 @@
+
+   
 from flask import Flask
 from flask import request
 import json
@@ -5,17 +7,19 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def hellow_world():
-    return "<p>Привет участникам электива<p>"
+
+@app.route("/",methods=['POST'])
+def hello_world():
+    return "<p>Привет участникам электива"
 
 @app.route("/parse/<job>")
-def parse_param(job):
+def parse_para(job):
     return "Вы обратились на {}".format(job)
 
-@app.route("/sample_post", methods=["POST"])
-def parse_param_post():
+@app.route("/sample_post",methods=["POST"])
+def parse_para_post():
     d = json.loads(request.data)
-    return "Вы обратились по посту на {}".format(d.['body'])
+    return "Вы обратились по посту на {}".format(d['body'])
 
-#app.run()
+
+###app.run()
